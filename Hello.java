096afd3797d25cfd133c7770e3451122fb545d11
Nen Pachani <nenpanchani@gmail.com>
2025-09-03 21:21:23 +0530

    Refactor comments in Hello.java for clarity and consistency

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        //Output
-        //print is use in single line Print statement
-        //println is use for print next statement in next line
+        // Output
+        // print is use in single line Print statement
+        // println is use for print next statement in next line
         System.out.println("Hello !!");
         System.out.println("Hello Nen!!");
+        System.out.print("*\n**\n***\n****");
 
     }
 }
