5305a056838f8152c1a546ed1ba4ea09b6e8713f
Nen Pachani <nenpanchani@gmail.com>
2025-09-03 23:41:50 +0530

    Implement user input handling and add methods for greeting and sum calculation

--- Conditional.java
+++ Conditional.java
@@ -4,12 +4,32 @@ public class Conditional {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int age = sc.nextInt();
+        /// int no1 = sc.nextInt();
+        // int no2 = sc.nextInt();
+        int button = sc.nextInt();
 
-        if (age > 18) {
-            System.out.println("You Are Adult");
-        } else {
-            System.out.println("You are Littele Baby");
+        // if (no1 == no2) {
+        // System.out.println("Equal");
+        // } else if (no1 > no2) {
+        // System.out.println("No1 Is Bigger");
+        // } else {
+        // System.out.println("No2 Is Bigger");
+        // }
+
+        switch (button) {
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bye");
+                break;
+
+            default:
+                System.out.println("Invalid Choice");
+                break;
         }
     }
 }
