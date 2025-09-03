39082c6c24a0fbf474094a3e28a278ca38f9a461
Nen Pachani <nenpanchani@gmail.com>
2025-09-03 21:17:08 +0530

    Add Hello.java with a simple greeting program

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        //Output
+        //print is use in single line Print statement
+        //println is use for print next statement in next line
+        System.out.println("Hello !!");
+        System.out.println("Hello Nen!!");
+
+    }
+}
