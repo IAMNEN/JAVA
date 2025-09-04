b1358fd289373211903451be2eeffab4c63c2804
Nen Pachani <nenpanchani@gmail.com>
2025-09-04 20:59:47 +0530

    Add Pra3 and Pra4 classes for sum calculation and multiplication table; update Forloop to print loop index

--- Forloop.java
+++ Forloop.java
@@ -1,7 +1,7 @@
 public class Forloop {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i = i + 1) {
-            System.out.println("Hello Java");
+            System.out.println(i);
         }
     }
 }
