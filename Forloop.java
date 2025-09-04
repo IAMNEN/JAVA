b5c24c89f2fb731183f174fdfa4a6337c75b0990
Nen Pachani <nenpanchani@gmail.com>
2025-09-04 20:35:09 +0530

    Add Forloop.java with a simple for loop printing "Hello Java"

---
+++ Forloop.java
@@ -0,0 +1,8 @@
+public class Forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i = i + 1) {
+            System.out.println("Hello Java");
+        }
+    }
+}
+// counter++ is work like add 1 in counter value
